66d99791494422808b8fc3295bb61eb060cf6709
manuel-seisl <1810653153@stud.fh-kufstein.ac.at>
2019-01-24 10:55:03 +0100

    Aufgabe IIII

--- src/Aufgabe3/Main.java
+++ src/Aufgabe3/Main.java
@@ -3,18 +3,21 @@ package Aufgabe3;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main
 {
 
     public static void main(String[] args){
 
-        Student a = new Student("Manuel", "Seisl", 26, 1810653153, "AT");
+        ArrayList<Student> students = new ArrayList<Student>();
+        students.add(new Student("Manuel", "Seisl", 26, 1810653153, "AT"));
+        students.add(new Student("Christof", "Jori", 22, 1810653154, "AT"));
 
         ObjectMapper obm = new ObjectMapper();
 
         try {
-            obm.writeValue(new File("test.json"), a);
+            obm.writeValue(new File("test.json"), students);
         } catch (IOException ex){
             ex.printStackTrace();
         }
